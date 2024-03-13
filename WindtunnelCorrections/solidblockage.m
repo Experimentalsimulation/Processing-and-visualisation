8888d3c43c15e76f96958545bb26d07c75bd5f66
Joep <Joepdeboer-3@student.tudelft.nl>
2024-03-13 13:55:38 +0100

    Besides the coefficients for the strusts solid blockage function is done

--- WindtunnelCorrections/solidblockage.m
+++ WindtunnelCorrections/solidblockage.m
@@ -2,9 +2,19 @@ function [epsilon] = solidblockage(Velocity)
 %SOLIDBLOCKAGE compute total epsilon for solid blockage of all parts
 % Input V [m/s] and output epsilon 
 
-% coefficients of all the parts
-% volumes of all the parts
+% coefficients of all the parts in order
+% fuselage, nacelles, wingstruts??, aft strut??, Wing, HT, VT 
+K = [0.908, 0.9302, 1, 1, 1.0475, 1.02, 1.035];
 
-epsilon =  0 % NOT IMPLMENTED
+% volumes of all the parts in same order
+V = [0.0160632, 0.0015842, 0.0035296, 0.0004491, 0.0030229, 0.0009751, 0.0003546]; % [m^3]
+
+%Tunnel shape factor 
+tau = 0.888;
+
+%Tunnel crossection 
+C = (1260 * 1800 - 2 * (300 * 300)) * 10^-6
+epsilon =  sum((K * tau * V)/C^(3/2)) % partially IMPLMENTED
+
 end
 
