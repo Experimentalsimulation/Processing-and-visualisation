608a4121c5cf6ca81602ab3f86f05859cc0d2304
Baptiste-van-hille <baptistevh2@gmail.com>
2024-03-30 12:03:14 +0100

    po

--- NEARFIELD MICS_NEW/Get_prms_corrected.m
+++ NEARFIELD MICS_NEW/Get_prms_corrected.m
@@ -1,16 +1,17 @@
 function [prms_corrected] = Get_prms_corrected(prms_on, prms_off, V_inf_experiment)
 
 
-%Substract prop off p_rms to prop on P_rms
-
+% %Substract prop off p_rms to prop on P_rms
+% 
 prms = prms_on - prms_off;
-
-
-
+% 
+% 
+% 
 %Correct for scaling
-
+% 
 V_inf_descent  = 149;  %Descent speed of Airvus a318 https://contentzone.eurocontrol.int/aircraftperformance/details.aspx?ICAO=A318
+% 
+prms_corrected = prms ;%.* (V_inf_descent/V_inf_experiment)^2;
 
-prms_corrected = prms .* (V_inf_descent/V_inf_experiment)^2;
 
 end
